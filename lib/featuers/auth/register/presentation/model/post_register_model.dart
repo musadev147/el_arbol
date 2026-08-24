d5a4b4d7aea51ff3405e1ac480fc0e1f374fb0c7
musamac <musasorkar95@gmail.com>
2026-08-24 21:44:38 +0600

    feat(wholesale): add wholesale B2B fields to PostRegisterModel

--- lib/featuers/auth/register/presentation/model/post_register_model.dart
+++ lib/featuers/auth/register/presentation/model/post_register_model.dart
@@ -32,26 +32,84 @@ class User {
   Profile? profile;
   String? userType;
 
-  User(
-      {this.id,
-      this.email,
-      this.username,
-      this.firstName,
-      this.lastName,
-      this.fullName,
-      this.profile,
-      this.userType});
+  // Wholesale specific fields
+  String? businessName;
+  String? contactName;
+  String? tradeLicenseNumber;
+  String? phone;
+  String? postcode;
+  String? businessType;
+  String? displayBusinessType;
+  String? monthlyVolume;
+  String? displayVolume;
+  String? status;
+  bool? isApproved;
+  String? appliedAt;
+  String? approvedAt;
+  String? accountManagerName;
+  String? accountManagerEmail;
+  int? totalOrders;
+  String? totalSpent;
+  String? profileImageUrl;
+
+  User({
+    this.id,
+    this.email,
+    this.username,
+    this.firstName,
+    this.lastName,
+    this.fullName,
+    this.profile,
+    this.userType,
+    this.businessName,
+    this.contactName,
+    this.tradeLicenseNumber,
+    this.phone,
+    this.postcode,
+    this.businessType,
+    this.displayBusinessType,
+    this.monthlyVolume,
+    this.displayVolume,
+    this.status,
+    this.isApproved,
+    this.appliedAt,
+    this.approvedAt,
+    this.accountManagerName,
+    this.accountManagerEmail,
+    this.totalOrders,
+    this.totalSpent,
+    this.profileImageUrl,
+  });
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     email = json['email'];
     username = json['username'];
-    firstName = json['firstName'];
-    lastName = json['lastName'];
-    fullName = json['fullName'];
-    profile =
-        json['profile'] != null ? Profile.fromJson(json['profile']) : null;
+    firstName = json['firstName'] ?? json['first_name'];
+    lastName = json['lastName'] ?? json['last_name'];
+    fullName = json['fullName'] ?? json['name'];
+    profile = json['profile'] != null ? Profile.fromJson(json['profile']) : null;
     userType = json['user_type'];
+
+    // Wholesale parsing
+    businessName = json['business_name'];
+    contactName = json['contact_name'];
+    tradeLicenseNumber = json['trade_license_number'];
+    phone = json['phone'];
+    postcode = json['postcode'];
+    businessType = json['business_type'];
+    displayBusinessType = json['display_business_type'];
+    monthlyVolume = json['monthly_volume'];
+    displayVolume = json['display_volume'];
+    status = json['status'];
+    isApproved = json['is_approved'];
+    appliedAt = json['applied_at'];
+    approvedAt = json['approved_at'];
+    accountManagerName = json['account_manager_name'];
+    accountManagerEmail = json['account_manager_email'];
+    totalOrders = json['total_orders'];
+    totalSpent = json['total_spent'];
+    profileImageUrl = json['profile_image_url'];
   }
 
   Map<String, dynamic> toJson() {
@@ -66,6 +124,26 @@ class User {
       data['profile'] = profile!.toJson();
     }
     data['user_type'] = userType;
+
+    // Wholesale serialization
+    data['business_name'] = businessName;
+    data['contact_name'] = contactName;
+    data['trade_license_number'] = tradeLicenseNumber;
+    data['phone'] = phone;
+    data['postcode'] = postcode;
+    data['business_type'] = businessType;
+    data['display_business_type'] = displayBusinessType;
+    data['monthly_volume'] = monthlyVolume;
+    data['display_volume'] = displayVolume;
+    data['status'] = status;
+    data['is_approved'] = isApproved;
+    data['applied_at'] = appliedAt;
+    data['approved_at'] = approvedAt;
+    data['account_manager_name'] = accountManagerName;
+    data['account_manager_email'] = accountManagerEmail;
+    data['total_orders'] = totalOrders;
+    data['total_spent'] = totalSpent;
+    data['profile_image_url'] = profileImageUrl;
     return data;
   }
 }
